3bf358ad7009f31c72165c1e6e00b047c21eb805
vladimir <kennyvova@yandex.ru>
2018-12-03 18:05:57 +0300

    modified

--- Vladimir_Zaykovskiy/src/index.jsx
+++ Vladimir_Zaykovskiy/src/index.jsx
@@ -2,6 +2,8 @@ import './app.sass';
 
 import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 
 import CommentSection from 'containers/CommentSection';
 
@@ -9,10 +11,12 @@ class App extends Component {
 	render() {
 		return (
       <Fragment>
-        <CommentSection />
-      </Fragment>
+				<Switch>
+					<Route path="/" exact component={CommentSection}/>
+				</Switch>
+			</Fragment>
 		)
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'));
